177df7bc6eb1e800ba0f52e8f45664da50d71fdc
HaiAnhTrinh <trinhhaianh37@gmail.com>
2018-04-16 01:31:19 +1000

    built welcome, register, confirm register UI.

--- app/src/main/java/com/example/troyphattrinh/fitness_app/MainActivity.java
+++ app/src/main/java/com/example/troyphattrinh/fitness_app/MainActivity.java
@@ -1,32 +1,37 @@
 package com.example.troyphattrinh.fitness_app;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    //used to debug and keep track of the steps
+    private static final String TAG = "importantMessage";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Log.i(TAG,"onCreate");
     }
+
+    public void clickRegButton(View v){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickLoginButton(View v){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
