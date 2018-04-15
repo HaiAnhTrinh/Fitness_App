96e8c84345254b3182217530fad14bac75383ab6
HaiAnhTrinh <trinhhaianh37@gmail.com>
2018-04-16 03:05:52 +1000

    built welcome, register, confirm register UI.

--- app/src/main/java/com/example/troyphattrinh/fitness_app/MainActivity.java
+++ app/src/main/java/com/example/troyphattrinh/fitness_app/MainActivity.java
@@ -4,9 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.util.Log;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,30 +27,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickLoginButton(View v){
-        Intent intent = new Intent(this, RegisterActivity.class);
+        Intent intent = new Intent(this, MainMenuActivity.class);
+
+        //adding extra data to another intent
+        final EditText usernameText = findViewById(R.id.usernameText);
+        String username = usernameText.getText().toString();
+        intent.putExtra("username", username);
+
         startActivity(intent);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/com/example/troyphattrinh/fitness_app/MainMenuActivity.java
+++ app/src/main/java/com/example/troyphattrinh/fitness_app/MainMenuActivity.java
@@ -2,6 +2,9 @@ package com.example.troyphattrinh.fitness_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        //enables the intent to get extra data from another activity
+        Bundle mainMenuData = getIntent().getExtras();
+        //if there is no extra data, don't do anything
+        if(mainMenuData==null){
+            return;
+        }
+        String username = mainMenuData.getString("username");
+
+        final TextView usernameText = findViewById(R.id.username);
+
+        usernameText.setText(username);
+
     }
 }
