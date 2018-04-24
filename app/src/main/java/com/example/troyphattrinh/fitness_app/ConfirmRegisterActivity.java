cbcd0971be3d18c7b166c9b7114212dee52d8fba
HaiAnhTrinh <trinhhaianh37@gmail.com>
2018-04-24 14:20:02 +1000

    built welcome, register, confirm register UI.

--- app/src/main/java/com/example/troyphattrinh/fitness_app/ConfirmRegisterActivity.java
+++ app/src/main/java/com/example/troyphattrinh/fitness_app/ConfirmRegisterActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ConfirmRegisterActivity extends AppCompatActivity {
 
@@ -11,10 +13,32 @@ public class ConfirmRegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_register);
-    }
 
+        Bundle confirmRegisterData = getIntent().getExtras();
+
+        if(confirmRegisterData == null){
+            return;
+        }
+
+        String confirmCode = confirmRegisterData.getString("confirmCode");
+
+        final EditText confirmText = findViewById(R.id.confirmText);
+        String confirmUser = confirmText.getText().toString();
+
+    }
+/*
     public void clickConfirmButton(View v){
         Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        TextView errorText = findViewById(R.id.errorTextView);
+
+
+        if(){
+            errorText.setText("");
+            startActivity(intent);
+        }
+        else{
+            errorText.setText("INCORRECT CONFIRMATION CODE!!!");
+        }
     }
+    */
 }
