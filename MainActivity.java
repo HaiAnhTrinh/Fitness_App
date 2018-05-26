6537b37a6a92bd7a13ed006c206b70773eb4806d
HoangNguyen5897 <31613471+HoangNguyen5897@users.noreply.github.com>
2018-05-26 21:45:08 +1000

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.hqng.dob;
+
+import android.app.DatePickerDialog;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String Tag = "MainActivity";
+
+    private EditText dob;
+    private DatePickerDialog.OnDateSetListener dateSetListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dob = findViewById(R.id.dob);
+
+        dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        MainActivity.this,
+                        dateSetListener,
+                        day, month, year);
+                dialog.show();
+            }
+        });
+
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month +1;
+                Log.d(Tag, "Select date: " + dayOfMonth + "/" + month + "/" + year);
+
+                String date = dayOfMonth + "/" + month + "/" + year;
+                dob.setText(date);
+            }
+
+        };
+    }
+
+
+}
