c39cb50a719d398e23a30501041143d71b7bea0f
HaiAnhTrinh <31613318+HaiAnhTrinh@users.noreply.github.com>
2018-05-27 23:00:21 +1000

    Update FootstepActivity.java

--- app/src/main/java/com/example/troyphattrinh/fitness_app/FootstepActivity.java
+++ app/src/main/java/com/example/troyphattrinh/fitness_app/FootstepActivity.java
@@ -3,6 +3,7 @@ package com.example.troyphattrinh.fitness_app;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,13 +14,18 @@ import android.widget.Toast;
 public class FootstepActivity extends Fragment implements StepSensorBase.StepCallBack {
     private TextView mStepText;
     private StepSensorBase mStepSensor;
-    private Button clearBtn;
+    private Button clearBtn, recordBtn;
     private StepSensorPedometer stepSensorPedometer;
     private boolean isRun = true;
+    private DatabaseHelper dbh;
+    static String email;
+    private int stepCounter;
+    private static final String TAG = "Footstep";
 
     @Override
     public void Step(int stepNum) {
         mStepText.setText("Steps:" + stepNum);
+        stepCounter = stepNum;
         int criterion = 10000;
         if(stepNum == criterion){
             Noti.showNotification(this);
@@ -37,12 +43,17 @@ public class FootstepActivity extends Fragment implements StepSensorBase.StepCal
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        dbh = new DatabaseHelper(this.getContext());
+        email = this.getArguments().getString("EMAIL_KEY");
+
         stepSensorPedometer = new StepSensorPedometer(this.getContext(), new StepSensorBase.StepCallBack() {
             @Override
             public void Step(int stepNum) {
 
             }
         });
+
+        recordBtn = view.findViewById(R.id.recordBtn);
 
         clearBtn = view.findViewById(R.id.clearBtn);
         if(isRun) {
@@ -55,6 +66,15 @@ public class FootstepActivity extends Fragment implements StepSensorBase.StepCal
         if (!mStepSensor.registerStep()) {
             Toast.makeText(getContext(), "Unavailable", Toast.LENGTH_SHORT).show();
         }
+
+        recordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbh.addSteps(stepCounter, email);
+                Log.i(TAG, "ACTION RECORD");
+
+            }
+        });
 
         clearBtn.setOnClickListener(new View.OnClickListener() {
             @Override
